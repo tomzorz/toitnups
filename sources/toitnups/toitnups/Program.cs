144458a1406d01e04c240dce8f0eff0874f36b07
Tamás Deme <tomzorz@outlook.com>
2019-05-07 10:53:44 +0200

    main and demo project init

---
+++ sources/toitnups/toitnups/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace toitnups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
